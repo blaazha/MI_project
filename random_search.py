92de73dc51009876435d5bf0cc3441e11172f75a
jan.blazek <jan.blazek10@gmail.com>
2024-04-14 20:41:42 +0200

    RS - Schweffel

--- random_search.py
+++ random_search.py
@@ -80,3 +80,39 @@ utils.plot_template('Průměr vyhledávání RS dejong 2 (10D)')
 
 # ----------------
 
+# RANDOM SEARCH 5D Schweffel
+rs_results = []
+for i in range(30):
+    _, rs_steps = searches.random_search(functions.schweffel, (-500, 500), 5, RANDOM_SEARCH_ITERATIONS)
+    rs_results.append(rs_steps)
+
+for rs_result in rs_results:
+    plt.plot([point[0] for point in rs_result],
+             [point[1] for point in rs_result], marker='', linestyle='-')
+
+utils.plot_template('Random search - jednotlivá spuštění Schweffel (5D)')
+
+# AVG STEPS RANDOM SEARCH 5D Schweffel
+rs_avg_steps = utils.average_score(rs_results)
+plt.plot([point[0] for point in rs_avg_steps],
+         [point[1] for point in rs_avg_steps], marker='', linestyle='-')
+utils.plot_template('Průměr vyhledávání RS Schweffel (5D)')
+
+# RANDOM SEARCH 10D Schweffel
+rs_results = []
+for i in range(30):
+    _, rs_steps = searches.random_search(functions.schweffel, (-500, 500), 10, RANDOM_SEARCH_ITERATIONS)
+    rs_results.append(rs_steps)
+
+for rs_result in rs_results:
+    plt.plot([point[0] for point in rs_result],
+             [point[1] for point in rs_result], marker='', linestyle='-')
+
+utils.plot_template('Random search - jednotlivá spuštění Schweffel (10D)')
+
+# AVG STEPS RANDOM SEARCH 10D Schweffel
+rs_avg_steps = utils.average_score(rs_results)
+plt.plot([point[0] for point in rs_avg_steps],
+         [point[1] for point in rs_avg_steps], marker='', linestyle='-')
+utils.plot_template('Průměr vyhledávání RS Schweffel (10D)')
+
